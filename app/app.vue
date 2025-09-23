6587dda8c4a702a1b320b84415f720c529ced293
Maxi <maximiliano.duran114@gmail.com>
2025-09-22 21:32:25 -0300

    first commit

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <header class="bg-blue-600 text-white p-4 shadow">
+      <h1 class="text-xl font-bold">Concurso del Hotel</h1>
+    </header>
+
+    <NuxtPage class="bg-red-400" />
+
+    <footer class="bg-gray-200 text-center p-4 mt-10">
+      © 2025 Hotel Concurso
+    </footer>
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="p-6 text-center">
+    <h1 class="text-2xl font-bold">Bienvenido a mi app Nuxt</h1>
+    <nuxt-link to="/register" class="text-blue-500 underline"
+      >Ir a Registro</nuxt-link
+    >
+  </div>
+</template>
